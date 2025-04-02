e1f44027914de417e0a7b2f7b5c01e7e752a0aa5
Szymon <szymers12345@gmail.com>
2025-04-02 23:04:43 +0200

    changed layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,8 +5,17 @@ import MainTemplate from "./templates/MainTemplate";
 function App() {
   return (
     <MainTemplate>
-      <GameBoard />
-      <PickNumber />
+      <div className="flex flex-col">
+        <div>Trudnosc</div>
+        <div className="flex gap-5">
+          <div>
+            <GameBoard />
+          </div>
+          <div>
+            <PickNumber />
+          </div>
+        </div>
+      </div>
     </MainTemplate>
   );
 }
