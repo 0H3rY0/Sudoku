e51dfb80cf6d56ab3b40f3dc0e9b655d228c32aa
Szymon <szymers12345@gmail.com>
2025-04-02 22:42:11 +0200

    game board changed styling

--- client/src/components/GameBoard.jsx
+++ client/src/components/GameBoard.jsx
@@ -1,21 +1,23 @@
 const GameBoard = () => {
   return (
-    <div className="w-[30rem] h-[30rem] border-4 border-gray-500">
+    <div className="w-[30rem] h-[30rem] border-4 border-black">
       <div className="grid grid-cols-9 grid-rows-9 h-full w-full">
-        {[...Array(81)].map((_, i) => (
-          <div
-            key={i}
-            className={`flex items-center justify-center border border-gray-400 text-xl font-bold w-full h-full
-            ${i % 9 === 2 || i % 9 === 5 ? "border-r-2 border-black" : ""}
-            ${
-              Math.floor(i / 9) === 2 || Math.floor(i / 9) === 5
-                ? "border-b-2 border-black"
-                : ""
-            }`}
-          >
-            {/* Tu wstawisz cyfry */}
-          </div>
-        ))}
+        {[...Array(81)].map((_, i) => {
+          const row = Math.floor(i / 9);
+          const col = i % 9;
+
+          return (
+            <div
+              key={i}
+              className={`flex items-center justify-center border border-gray-400 text-xl font-bold w-full h-full
+              ${col % 3 === 2 && col !== 8 ? "border-r-4 border-black" : ""} 
+              ${row % 3 === 2 && row !== 8 ? "border-b-4 border-black" : ""}
+              `}
+            >
+              {/* Tu wstawisz cyfry */}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
