c7d450c75026415fbcb6dddf43ba58503e1f9705
Szymon <szymers12345@gmail.com>
2025-04-02 22:58:01 +0200

    add PickNumber component with new button component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import GameBoard from "./components/GameBoard";
+import PickNumber from "./components/PickNumber";
 import MainTemplate from "./templates/MainTemplate";
 
 function App() {
   return (
     <MainTemplate>
       <GameBoard />
+      <PickNumber />
     </MainTemplate>
   );
 }
---
+++ client/src/components/NewGameButton.jsx
@@ -0,0 +1,9 @@
+const NewGameButton = () => {
+  return (
+    <button className="w-full h-16 text-white bg-blue-800 rounded-md font-extrabold">
+      New Game
+    </button>
+  );
+};
+
+export default NewGameButton;
