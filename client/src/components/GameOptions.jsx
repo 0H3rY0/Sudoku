54877831f258cab8d129de34df023ffeb507dee3
Szymon <szymers12345@gmail.com>
2025-07-13 21:22:03 +0200

    added visual presentation of  mistakes and time to GameOptions

--- client/src/components/GameOptions.jsx
+++ client/src/components/GameOptions.jsx
@@ -8,26 +8,39 @@ const GameOptions = () => {
   const { clearPickedMove, undoMove } = useBoard();
 
   return (
-    <div className="flex gap-5 w-full text-center mb-2 justify-center text-blue-500 font-extrabold">
-      <div
-        className="w-14 h-14 flex items-center justify-center bg-purple-200 rounded-full hover:bg-purple-300"
-        onClick={() => undoMove()}
-      >
-        <MdOutlineRefresh size={32} />
+    <>
+      <div className="flex justify-between mb-2 ">
+        <div>
+          <p className="text-gray-400 font-extrabold text-md flex ">
+            Mistakes: 1/3
+          </p>
+        </div>
+        <div className="text-gray-400 font-extrabold text-md flex">
+          Time: 00:01
+        </div>
       </div>
-      <div
-        className="w-14 h-14 flex items-center justify-center bg-purple-200 rounded-full hover:bg-purple-300"
-        onClick={() => clearPickedMove()}
-      >
-        <PiEraser size={32} />
+      <div></div>
+      <div className="flex gap-5 w-full text-center mb-2 justify-center items-center text-blue-500 font-extrabold">
+        <div
+          className="w-14 h-14 flex items-center justify-center bg-purple-200 rounded-full hover:bg-purple-300"
+          onClick={() => undoMove()}
+        >
+          <MdOutlineRefresh size={32} />
+        </div>
+        <div
+          className="w-14 h-14 flex items-center justify-center bg-purple-200 rounded-full hover:bg-purple-300"
+          onClick={() => clearPickedMove()}
+        >
+          <PiEraser size={32} />
+        </div>
+        <div className="w-14 h-14 flex items-center justify-center bg-purple-200 rounded-full hover:bg-purple-300">
+          <BsPencil size={32} />
+        </div>
+        <div className="w-14 h-14 flex items-center justify-center bg-purple-200 rounded-full hover:bg-purple-300">
+          <FaRegLightbulb size={32} />
+        </div>
       </div>
-      <div className="w-14 h-14 flex items-center justify-center bg-purple-200 rounded-full hover:bg-purple-300">
-        <BsPencil size={32} />
-      </div>
-      <div className="w-14 h-14 flex items-center justify-center bg-purple-200 rounded-full hover:bg-purple-300">
-        <FaRegLightbulb size={32} />
-      </div>
-    </div>
+    </>
   );
 };
 
