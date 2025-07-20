834657b499f7a206f40d471cde25519e2b68315c
Szymon <szymers12345@gmail.com>
2025-07-20 22:22:01 +0200

    added basic modal structure

--- client/src/components/NewGameButton.jsx
+++ client/src/components/NewGameButton.jsx
@@ -1,8 +1,12 @@
+import Test from "./modals/test";
+
 const NewGameButton = () => {
   return (
-    <button className="w-full h-16 text-white bg-blue-900 rounded-md font-extrabold hover:bg-blue-950">
-      New Game
-    </button>
+    <Test>
+      <button className="w-full h-16 text-white bg-blue-900 rounded-md font-extrabold hover:bg-blue-950">
+        New Game
+      </button>
+    </Test>
   );
 };
 
---
+++ client/src/components/modals/test.jsx
@@ -0,0 +1,35 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import { IoClose } from "react-icons/io5";
+
+const Test = ({ children, text, btnText, func }) => {
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger asChild>{children}</Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className="modal-overlay">
+          <Dialog.Content className="modal-content">
+            <div className="modal-header">
+              <p>{text}</p>
+              <Dialog.Close className="modal-close">
+                <IoClose size={28} />
+              </Dialog.Close>
+            </div>
+
+            <div className="modal-actions">
+              <Dialog.Close asChild>
+                <button className="button bg-custom-blue hover:bg-custom-blue-light">
+                  Anuluj
+                </button>
+              </Dialog.Close>
+              <button className="button bg-custom-orange" onClick={func}>
+                {btnText}
+              </button>
+            </div>
+          </Dialog.Content>
+        </Dialog.Overlay>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+};
+
+export default Test;
