f168282c71d65a6fde94ead54fac8e553a79ad3b
Szymon <szymers12345@gmail.com>
2025-07-21 19:30:24 +0200

    modal styling

--- client/src/components/modals/test.jsx
+++ client/src/components/modals/test.jsx
@@ -1,29 +1,42 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { IoClose } from "react-icons/io5";
+import { BsClipboard2Check } from "react-icons/bs";
 
-const Test = ({ children, text, btnText, func }) => {
+const Test = ({ children }) => {
   return (
     <Dialog.Root>
       <Dialog.Trigger asChild>{children}</Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className="modal-overlay">
           <Dialog.Content className="modal-content">
-            <div className="modal-header">
-              <p>{text}</p>
+            <div className="absolute left-[88%] top-[2%]">
               <Dialog.Close className="modal-close">
                 <IoClose size={28} />
               </Dialog.Close>
             </div>
 
-            <div className="modal-actions">
-              <Dialog.Close asChild>
-                <button className="button bg-custom-blue hover:bg-custom-blue-light">
-                  Anuluj
-                </button>
-              </Dialog.Close>
-              <button className="button bg-custom-orange" onClick={func}>
-                {btnText}
-              </button>
+            <div className="flex justify-center items-center flex-col">
+              <h2 className="font-bold text-xl text-slate-600 mb-5">
+                Choose game mode
+              </h2>
+              <ul className="bg-slate-200 rounded-lg font-semibold text-slate-600">
+                <li className="list-item">
+                  <BsClipboard2Check size={26} />
+                  Easy
+                </li>
+                <li className="list-item">
+                  <BsClipboard2Check size={26} />
+                  Medium
+                </li>
+                <li className="list-item">
+                  <BsClipboard2Check size={26} />
+                  Hard
+                </li>
+                <li className="list-item">
+                  <BsClipboard2Check size={26} />
+                  Killer
+                </li>
+              </ul>
             </div>
           </Dialog.Content>
         </Dialog.Overlay>
